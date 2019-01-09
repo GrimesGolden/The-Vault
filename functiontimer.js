360f1dad3b6141221a166d695cc4ba3e518b0485
GrimesGolden <46364302+GrimesGolden@users.noreply.github.com>
2019-01-09 14:54:22 +0900

    Create functiontimer.js

---
+++ functiontimer.js
@@ -0,0 +1,11 @@
+//This will show you how long a function takes to run. Powerful stuff... 
+const timeFuncRuntime = funcParameter => {
+   let t1 = Date.now();
+   funcParameter();
+   let t2 = Date.now();
+   return t2 - t1;
+}
+
+const addOneToOne = () => 1 + 1;
+
+timeFuncRuntime(addOneToOne);
